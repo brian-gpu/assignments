2af81fee797a3d46f657e08bca8628ce47048310
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-02-21 16:56:49 -0500

    add assignment 1 and 2

---
+++ assignment2/day2-exercise1-3.py
@@ -0,0 +1,23 @@
+# Exercise 1
+# 50+50 is one number and 100-10 is another
+print("Exercise 1")
+print("Assumption: 50+50 is one number and 100-10 is another")
+number1 = 50 + 50
+number2 = 100 - 10
+print(number1 + number2)
+print('\n')
+
+# Exercise 2
+# Error occurs due to first asterisk, if asterisk
+# is removed it prints the values 36, 0, 46656, and 36
+print("Exercise 2")
+print('''Error occurs due to first asterisk, if asterisk is removed 
+it prints the values 36, 0, 46656, and 36''')
+print(30+6,6^6,6**6,6+6+6+6+6+6)
+print('\n')
+
+# Exercise 3
+print("Exercise 3")
+print('Hello World')
+print('output “Hello World : 10”')
+print('\n')
