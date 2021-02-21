06440229e303269b0b061b8b087339994e37db9a
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-02-21 17:16:18 -0500

    add assignment 3

---
+++ assignment3/3-exercise1-4.py
@@ -0,0 +1,21 @@
+# Exercise 1
+print("Exercise 1")
+print('Hello World'[8])
+print('\n')
+
+# Exercise 2
+print("Exercise 2")
+print('thinker'[2:5])
+print("h[1] is not defined, error")
+print('\n')
+
+# Exercise 3
+print("Exercise 3")
+print("output of s[2:] is 'mmy'")
+print('\n')
+
+# Exercise 4
+print("Exercise 4")
+print("Yes")
+print(set('Mississippi'))
+print('\n')
