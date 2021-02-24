1ca69d26cf9f3e03de20b3cf21c512083551b05d
brian-gpu <79373206+brian-gpu@users.noreply.github.com>
2021-02-23 20:15:23 -0500

    remove previous files

--- assignment4/day4-q4.py
+++
@@ -1,6 +0,0 @@
-numbers = input()
-list_of_nums = numbers.split(',')
-tuple_of_nums = tuple(list_of_nums)
-
-print(list_of_nums)
-print(tuple_of_nums)
--- assignment7/7-exercise6.py
+++
@@ -1,5 +0,0 @@
-def reverse_word(word):
-    return word[::-1]
-
-word = input("Input a word:\n")
-print(reverse_word(word))
